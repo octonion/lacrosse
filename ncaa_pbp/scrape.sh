5c8208ad8fd860f0c73c75dcd984610b8d0ddf5c
Christopher D. Long <galizur@gmail.com>
2018-04-01 20:16:09 -0400

    NCAA lacrosse update.

--- ncaa_pbp/scrape.sh
+++ ncaa_pbp/scrape.sh
@@ -5,8 +5,8 @@
 #year=$1
 #division=$2
 
-#./scrapers/ncaa_team_rosters.rb $1 $2
-#./scrapers/ncaa_summaries.rb $1 $2
-#./scrapers/ncaa_team_schedules.rb $1 $2
+./scrapers/ncaa_team_rosters.rb $1 $2
+./scrapers/ncaa_summaries.rb $1 $2
+./scrapers/ncaa_team_schedules.rb $1 $2
 ./scrapers/ncaa_box_scores.rb $1 $2
 ./scrapers/ncaa_play_by_play.rb $1 $2
